c01e50a6e7bc172e47953e30c17e34d5fe8d6947
Mohcine <momelloul@partner.auchan.fr>
2021-12-07 12:02:28 +0100

    init

---
+++ src/mock/index.js
@@ -0,0 +1,7 @@
+export const mock = {
+  label: 'KIRI Fromage fondu à la crème en portion 24 portions 432g',
+  salePrice: '4,49€',
+  purchasePrice: '4,19€',
+  ean: '3073781131963',
+  univers: ''
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,107 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  <IonPage>
+    <IonContent class="ion-text-center">
+      <div class="top-wrapper">
+        <div>
+          <img alt="" src="assets/barcode.gif">
+        </div>
+        <ion-input class="ion-input" clear-input placeholder="Saisir le code-barres EAN manuellement"
+                   value=""></ion-input>
+        <div id="or">OU</div>
+        <IonButton class="ion-button" expand="block" @click="openScanner">Scanner le code-barres</IonButton>
       </div>
-    </ion-content>
-  </ion-page>
+      <div class="bottom-wrapper">
+        <h3>Scans récents</h3>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>{{ codeBar }}</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            Lorem ipsum
+          </ion-card-content>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>Title</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            Lorem ipsum
+          </ion-card-content>
+        </ion-card>
+      </div>
+    </IonContent>
+  </IonPage>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script lang="js">
+  import { IonContent, IonPage, IonButton, IonText } from '@ionic/vue'
+  import { defineComponent } from 'vue'
+  import { BarcodeScanner } from '@ionic-native/barcode-scanner'
+  import { close, scanOutline, pin } from 'ionicons/icons'
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
+
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      IonContent,
+      IonPage,
+      IonButton
+    },
+    data() {
+      return {
+        codeBar: '',
+        pin,
+        close,
+        scanOutline
+      }
+    },
+    methods: {
+      async openScanner() {
+        const data = await BarcodeScanner.scan()
+        this.codeBar = data.text
+      }
+    }
+  })
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+<style lang="scss" scoped>
+  .ion-input {
+    border: 1px solid #C3C3C3;
+    border-radius: 30px;
+  }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+  .ion-button {
+    --border-radius: 30px;
+    margin: 0 40px
+  }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
+  .top-wrapper {
+    padding: 0 1em;
+  }
 
-#container a {
-  text-decoration: none;
-}
+  .bottom-wrapper {
+    background-image: linear-gradient(#DDEDFA, white);
+    margin-top: -23px;
+    min-height: 60%;
+    padding: 60px 1em 1em;
+    text-align: left;
+  }
+
+  #or {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #C3C3C3;
+    margin: 40px auto;
+
+    &::before, &::after {
+      content: "";
+      display: block;
+      background: #C3C3C3;
+      width: 30%;
+      height: 1px;
+      margin: 0 10px;
+    }
+  }
 </style>
